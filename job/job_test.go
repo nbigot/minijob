4cd55cacea8e5cafbcaf122838aab8114c22c43d
Nicolas Bigot <nicbigot@gmail.com>
2025-12-30 16:51:21 +0100

    test: fix test by adding callbackUrl field to Job JSON marshaling

--- job/job_test.go
+++ job/job_test.go
@@ -103,7 +103,7 @@ func TestJob_MarshalJSON(t *testing.T) {
 		state:         JobPending,
 	}
 
-	expectedJSON := `{"id":"0190e951-c29a-70aa-ba59-18be4abe9700","topic":"topic1","priority":0,"properties":{"key1":"value1","key2":42,"key3":true,"key4":["a","b","c"]},"history":[],"lockResources":["resource1","resource2","resource3:abc:def"],"userAgent":"user agent test","requester":"unit-test","name":"","sessionId":"0190e951-c29a-70aa-ba59-18be4abe97a1","traceId":"0190e951-c29a-70aa-ba59-18be4abe97a0","visibilityTimeout":0,"startAfter":0}`
+	expectedJSON := `{"id":"0190e951-c29a-70aa-ba59-18be4abe9700","topic":"topic1","priority":0,"properties":{"key1":"value1","key2":42,"key3":true,"key4":["a","b","c"]},"history":[],"lockResources":["resource1","resource2","resource3:abc:def"],"userAgent":"user agent test","requester":"unit-test","name":"","sessionId":"0190e951-c29a-70aa-ba59-18be4abe97a1","traceId":"0190e951-c29a-70aa-ba59-18be4abe97a0","visibilityTimeout":0,"startAfter":0,"callbackUrl":""}`
 
 	resultJSON, err := json.Marshal(j)
 	if err != nil {
